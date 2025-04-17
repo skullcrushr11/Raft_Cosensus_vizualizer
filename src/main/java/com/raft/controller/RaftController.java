dc50dd3fd3e15098460e0a560cd459ae087e5cc9
skullcrushr11 <saitarun575@gmail.com>
2025-04-17 16:22:10 +0530

    changes for deployment

--- src/main/java/com/raft/controller/RaftController.java
+++ src/main/java/com/raft/controller/RaftController.java
@@ -1,28 +1,13 @@
 package com.raft.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RaftController {
-
+    
     @GetMapping("/")
-    public String index(Model model) {
+    public String index() {
         return "index";
-    }
-
-    @PostMapping("/start-election")
-    public String startElection() {
-        // Implement election logic here
-        return "redirect:/";
-    }
-
-    @PostMapping("/add-log")
-    public String addLogEntry(@RequestParam String entry) {
-        // Implement log entry logic here
-        return "redirect:/";
     }
 } 
